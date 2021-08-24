032d8110c4a205551b03487aa4549c6b0b158e17
Sadra Saderi <sadrassx@gmail.com>
2021-08-24 17:23:00 +0430

    Add a dummy assignment
    Co-authored-by: Mad Scientist <aeirya@gmail.com>

--- res/js/file.js
+++ res/js/file.js
@@ -5,7 +5,7 @@ function readFile(path) {
 
             reader.onload = () => {
                 resolve(reader.result);
-            }
+            };
 
             file.blob().then(blob => {
                 reader.readAsText(blob);
@@ -14,14 +14,40 @@ function readFile(path) {
     });
 }
 
+function addElements(asg, text) {
+    text = text.replaceAll('<m-code>', '<pre><code>');
+    text = text.replaceAll('</m-code>', '</code></pre>');
+
+    text = text.replaceAll('<m-text>', '<p>');
+    text = text.replaceAll('</m-text>', '</p>');
+
+    asg.innerHTML = text;
+
+    document.querySelectorAll('m-box').forEach(obj => {
+        console.log('hi');
+
+        let problem = $(obj).attr('problem');
+        let value = $(obj).text();
+        obj.remove();
+
+        let e = asg.appendChild(box.cloneNode(true));
+        e.querySelectorAll('*[pid]').forEach(object =>
+            object.setAttribute('pid', problem));
+        e.querySelector('textarea').value = value;
+    });
+
+
+    hljs.highlightAll();
+}
+
 var item;
 $(() => {
-    document.querySelectorAll('div[asg]').forEach((obj) => {
+    document.querySelectorAll('div[asg]').forEach(obj => {
         let $this = $(obj);
 
         let asg = $this.attr('asg');
         readFile(asg).then(text => {
-            $this.html(text);
+            addElements($this.get(0), text);
         });
     });
 });
--- res/js/main.js
+++ res/js/main.js
@@ -1,3 +1,30 @@
+var box = null;
+
+function codeInputChange(e) {
+    let file = e.files[0];
+
+    let reader = new FileReader();
+
+    reader.onload = () => {
+        $('textarea[pid=' + $(e).attr('pid') + ']')
+            .get(0).value = reader.result;
+    };
+
+    try {
+        reader.readAsText(file);
+    } catch (error) { }
+
+    //// e.value = '';
+}
+
 $(() => {
+    let temp = document.getElementById('box-temp');
+
+    box = temp.cloneNode(true);
+    box.classList.remove('visually-hidden');
+    box.removeAttribute('id');
+
+    temp.remove();
+
     hljs.highlightAll();
 });
